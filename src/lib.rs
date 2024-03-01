e3b8a8c8ad14387cdfb2276cc01baf18d230d37e
Mikael Nääs <naasmikael@gmail.com>
2024-03-01 21:44:24 +0100

    add some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,36 @@
+//! ## Example
+//!
+//! Setup:
+//! ```rs
+//! use bevy_contact_projective_decals::{DecalPlugin};
+//! fn main() {
+//!     App::new()
+//!         .add_plugins((DefaultPlugins, DecalPlugin))
+//!         .run();
+//! }
+//! ```
+//!
+//! Spawning:
+//! ```rs
+//! commands.spawn(DecalBundle {
+//!     transform: Transform::from_xyz(x, y, z),
+//!     decal_material: decal_materials.add(ExtendedMaterial::<
+//!         StandardMaterial,
+//!         DecalMaterial,
+//!     > {
+//!         base: StandardMaterial {
+//!             base_color_texture: Some(asset_server.load("my_decal.png"))),
+//!             base_color: color,
+//!             alpha_mode: AlphaMode::Blend,
+//!             ..default()
+//!         },
+//!         extension: DecalMaterial {
+//!             depth_fade_factor: 8.0,
+//!         },
+//!     }),
+//!     mesh: meshes.add(decal_mesh_quad(Vec2::splat(scale))),
+//!     ..default()
+//! });
+//! ```
 mod decal;
 pub use decal::{decal_mesh_quad, DecalBundle, DecalMaterial, DecalPlugin};
